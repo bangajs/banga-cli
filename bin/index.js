35f8c74461af7c99f97686657e0a908d5ea2be0c
saucecodee <ashawinta@gmail.com>
2020-09-09 06:21:11 +0100

    feat: name format and args util

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-const args = process.argv
+const args = require("./../lib/utils/args")
+const banga = require("./../lib")
 
-require("./../lib")(args)
+banga(args.parse(process.argv))
