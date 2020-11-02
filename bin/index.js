9f211b9e544b6af2841d2cc5d01612de42aa3580
saucecodee <ashawinta@gmail.com>
2020-11-02 01:16:48 +0100

    chore: make error messages red

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const help = require("./../lib/cmd/help")
 
 try {
      require("./../lib/utils/app").parse()
      if (process.ARGS) require("./../lib")()
 } catch (error) {
-     console.log(error.message)
+     console.log(chalk.red(error.message))
      help()
 }
