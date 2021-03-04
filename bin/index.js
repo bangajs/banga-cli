ab5e20388be685dcdbea9a8b9ea9e8f927ce0061
saucecodee <ashawinta@gmail.com>
2021-03-04 23:26:31 +0100

    fix code indexing

--- bin/index.js
+++ bin/index.js
@@ -4,9 +4,9 @@ const chalk = require('chalk');
 const help = require("./../lib/cmd/help")
 
 try {
-     require("./../lib/utils/app").parse()
-     if (process.ARGS) require("./../lib")()
+   require("./../lib/utils/app").parse()
+   if (process.ARGS) require("./../lib")()
 } catch (error) {
-     console.log(chalk.red(error.message))
-     help()
+   console.log(chalk.red(error.message))
+   help()
 }
