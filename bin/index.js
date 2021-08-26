d9180763cb66f24f1cca1a75436b21920465e48d
saucecodee <ashawinta@gmail.com>
2021-08-26 01:39:21 +0100

    feat: handle "unhandledRejection" errors

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
+const chalk = require("chalk");
 const help = require("./../lib/cmd/help")
 
+process.title = "bangajs-cli";
+process.on("unhandledRejection", (err) => console.log( chalk.red(err.message)));
+
 try {
-   require("./../lib/utils/app").parse()
-   if (process.ARGS) require("./../lib")()
+  require("./../lib/utils/app").parse()
+  if (process.ARGS) require("./../lib")()
 } catch (error) {
-   console.log(chalk.red(error.message))
-   help()
+  console.log(chalk.red(error.message))
+  help()
 }
