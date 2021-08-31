abd31bb96a8fddb4f82c7e39f57188ccba0fc609
saucecodee <ashawinta@gmail.com>
2021-08-31 02:40:48 +0100

    wip: refactor code structure

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+"use strict"
 
 const chalk = require("chalk");
 const help = require("./../lib/cmd/help")
@@ -7,7 +8,7 @@ process.title = "bangajs-cli";
 process.on("unhandledRejection", (err) => console.log( chalk.red(err.message)));
 
 try {
-  require("./../lib/utils/app").parse()
+  require("../lib/bootstrap").parse()
   if (process.ARGS) require("./../lib")()
 } catch (error) {
   console.log(chalk.red(error.message))
